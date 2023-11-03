49d211427d53709ccc369b0683251656356184b1
Alan Wong <alanwongis@gmail.com>
2023-11-03 13:11:23 -0700

    Ignore Non-field lines

--- doc2csv.py
+++ doc2csv.py
@@ -1,12 +1,30 @@
 from csv import DictWriter
 import glob
 import pprint
+import re
 
 from docx import Document
 
+ignore = [
+    r"This report was prepared on",
+    r"Trademark Application / Registration Summary Report"
+]
 
-def starts_with_field_name(par, field_delim):
-    """trys to find the field name at the start of the paragraph
+path = "docx/Trade*.docx"
+csv_filename = "kls_files.csv"
+
+
+def is_not_ignore(st:str):
+    "check if a string should be ignored"
+    status = True
+    for ig in ignore:
+        if st.find(ig) >=0:
+            status = False
+    return status
+
+
+def starts_with_field_name(st, field_delim):
+    """tries to find the field name at the start of the paragraph
     Returns True if found
     """
     return par.text.find(field_delim) >= 0
@@ -24,20 +42,22 @@ def docx_to_dict(filename: str, field_delim=":"):
 
     doc = Document(filename)
     for p in doc.paragraphs:
+        text = p.text
         # on a transition to a different field
-        if starts_with_field_name(p, field_delim):
-            # create a key data pair from all the lines
+        if is_not_ignore(text) and text.find(field_delim)>=0:
+            # create a key data pair from all the previous lines
             d[curr_key] = "\n".join([lin.strip() for lin in lines])
+            print(curr_key,":", d[curr_key])
             # and set up for following lines of data
             lines = []
-            chunks = p.text.split(field_delim)
+            chunks = text.split(field_delim)
             curr_key = chunks[0].strip()
             lines.append(field_delim.join(chunks[1:]))
         else:
-            lines.append(p.text.strip())
+            lines.append(text.strip())
     # handle last remaining key
     d[curr_key] = "\n".join([lin.strip() for lin in lines])
-    del d[None]
+    del d[None] # remove dummy key
     return d
 
 
@@ -64,9 +84,7 @@ def scan_folder(path: str, csv_filename: str):
     csv_file.close()
 
 
-path = "docx/*.docx"
-csv_filename = "kls_files.csv"
+if __name__ == "__main__":
 
-
-scan_folder(path, csv_filename)
+    scan_folder(path, csv_filename)
 
